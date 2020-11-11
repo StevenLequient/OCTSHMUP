6e37e7b2c0931445642b419a0c79dae35d0b3f8f
Epsiom <steven06007@gmail.com>
2020-11-11 18:50:00 +0100

    Constants added

--- MAGJ2020/Assets/Scripts/GameHandler.cs
+++ MAGJ2020/Assets/Scripts/GameHandler.cs
@@ -8,7 +8,8 @@ public class GameHandler : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Debug.Log(Utils.Constants.PLAYER_SPEED);
+        //audioManager.Play("MainTheme");
     }
 
     // Update is called once per frame
