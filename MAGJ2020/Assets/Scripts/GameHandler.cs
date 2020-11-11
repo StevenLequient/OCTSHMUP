a2947844c0c5fca0872ab375819c1d2e772a7d7f
Epsiom <steven06007@gmail.com>
2020-11-11 18:42:17 +0100

    Project initialization and music

---
+++ MAGJ2020/Assets/Scripts/GameHandler.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+
+public class GameHandler : MonoBehaviour
+{
+    public AudioManager audioManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
