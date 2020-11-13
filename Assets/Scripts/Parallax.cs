5c9fde018e5a833b7a169d115d1e05a36191334b
Epsiom <steven06007@gmail.com>
2020-11-14 00:07:34 +0100

    Prefab horizontal parallax

---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    private float length, startPos;
+    public float parallaxEffect;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.localPosition.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float newPos = startPos + (parallaxEffect * Time.time) % length;
+        transform.localPosition = new Vector3(newPos, transform.localPosition.y, transform.localPosition.z);
+    }
+}
