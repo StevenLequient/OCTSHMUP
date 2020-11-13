702f3409d2c77af8e5b1127efb1e0ea5dc6b78ed
Robin Alonzo <alonzo.robin7@gmail.com>
2020-11-13 22:02:58 +0100

    Tetris! kinda...

---
+++ Assets/TetrisController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TetrisController : MonoBehaviour
+{
+    public Boolean directControl;
+    public GameObject movingTetromino;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (directControl)
+        {
+            var tetromino = movingTetromino.GetComponent<Tetromino>();
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                tetromino.MoveLeft();
+            }
+            else if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                tetromino.MoveRight();
+            }
+        }
+    }
+}
