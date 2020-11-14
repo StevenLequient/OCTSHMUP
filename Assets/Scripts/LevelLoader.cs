d115fa79239a1ad97405ddd1fca562e588ca982a
Epsiom <steven06007@gmail.com>
2020-11-14 19:27:28 +0100

    Scene transition

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public Animator transition;
+    public float transitionTime = 1f;           // The current animation length is 1 second
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadNextLevel()
+    {
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));        // The function is an asynchronous coroutine, so it's wrapped in StartCoroutine       
+    }
+
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        
+        transition.SetTrigger("Start");                         // Sets off the trigger of the animator which plays the animation
+        yield return new WaitForSeconds(transitionTime);        // Wait for the animation to complete
+        SceneManager.LoadScene(levelIndex);                     // Loads the next level scene from the scene build settings
+    }
+
+}
