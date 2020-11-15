b3cf68728cc5d893d8489484d96fddad4fc477b2
Mathieu Merino <mmerino@outlook.fr>
2020-11-15 15:49:42 +0100

    update tetris ui

--- Assets/Scripts/TetrisUI.cs
+++ Assets/Scripts/TetrisUI.cs
@@ -7,12 +7,21 @@ public class TetrisUI : MonoBehaviour
 {
     private TextMeshProUGUI levelCountTM;
     private TextMeshProUGUI lineCountTM;
+    private TextMeshProUGUI scoreCountTM;
 
     // Start is called before the first frame update
     void Start()
     {
         levelCountTM = transform.Find("LevelCount").GetComponent<TextMeshProUGUI>();
         lineCountTM = transform.Find("LineCount").GetComponent<TextMeshProUGUI>();
+        scoreCountTM = transform.Find("ScoreCount").GetComponent<TextMeshProUGUI>();
+    }
+
+    void Update()
+    {
+        levelCountTM.text = "" + TetrisController.Instance.level;
+        lineCountTM.text = "" + TetrisController.Instance.clearedLines;
+        scoreCountTM.text = "" + TetrisController.Instance.score;
     }
 
     public void ChangeLevelCount(int newLevelCount)
