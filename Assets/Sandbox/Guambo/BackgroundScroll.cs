9daeb5d542b8bd86f35ac3dc8618036c1d0a25b5
Nick Vallejos <nvallejos123@gmail.com>
2020-11-15 12:09:42 -0500

    Got background scrolling working

--- Assets/Sandbox/Guambo/BackgroundScroll.cs
+++ Assets/Sandbox/Guambo/BackgroundScroll.cs
@@ -5,23 +5,34 @@ using UnityEngine;
 public class BackgroundScroll : MonoBehaviour
 {
     public float scrollSpeed = 5.0f;
-    private Vector3 startPosition = Vector3.zero;
+    public Transform respawnPosition = null;
     public Transform endPositionTransform = null;
-    public Vector3 endPosition = Vector3.zero;
+    private Vector3 startPosition = Vector3.zero;
+    private Vector3 endPosition = Vector3.zero;
+
+    public List<GameObject> backgrounds;
 
     // Start is called before the first frame update
     void Start()
     {
-        startPosition = transform.position;
+        startPosition = respawnPosition.position;
         endPosition = endPositionTransform.position;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        transform.position = Vector3.MoveTowards(transform.position, endPosition, scrollSpeed * Time.deltaTime);
-        if (transform.position == endPosition) {
-            transform.position = startPosition;
+        MoveBackgrounds();
+    }
+    void MoveBackgrounds() {
+        foreach (GameObject background in backgrounds) {
+            background.transform.Translate(Vector2.up * -1 * scrollSpeed * Time.deltaTime);
         }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.tag == "background") {
+            collision.gameObject.transform.position = startPosition;
+        }    
     }
 }
